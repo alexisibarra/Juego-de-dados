c57247929ded64782661fd2ac8682c833a6243a4
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2013-07-14 18:31:02 -0430

    Implementación de la versión de hilos del juego. Vacía para este commit

---
+++ juegodedados_h.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  juegodedados_h.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  14/07/13 18:28:58
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alexis Ibarra (ai), ar.ibarrasalas@gmail.com
+ *   Organization:  Universidad Simón Bolívar
+ *
+ * =====================================================================================
+ */
+#include <errno.h>
+#include "funciones.h"
+
